1f9af5d5af81eb7be98cab2b930676e793c54083
Nobita <58627889+Nobita22@users.noreply.github.com>
2023-02-25 21:38:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -882,3 +882,24 @@ function openR() {
   v.style.display = "block";
 }
 // home page------------------------------------------------------------------------------------------------------------------------------------
+//--------------joy boy-------------
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+//---------------
